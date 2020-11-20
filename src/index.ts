81fee710e91abbab49704c7c1686ba4654c60683
mikohan <angara99@gmail.com>
2020-11-20 22:40:10 +0300

    Done whith typescript webpack configuration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-import { second } from './second';
-// import './scss/index.scss';
 import './styles/index.scss';
-console.log('Hello typescript from Index');
-
-second('Say hello from webpack NOW');
+console.log('Typescript Simple Webpack Config');
--- src/second.ts
+++
@@ -1,5 +0,0 @@
-console.log('from second file');
-
-export const second: (param: string) => void = (param) => {
-  console.log(`${param} from second file`);
-};
