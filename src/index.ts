3bbd81dcb65496e01abfdc7bc9a3a899df4937bc
mikohan <angara99@gmail.com>
2020-11-21 16:26:51 +0300

    Tweaks done

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
 import './styles/index.scss';
-import { MyClass } from '~/newDelete';
-// import '../assets/favicon.png';
-
-console.log('Typescript Simple Webpack Config');
-
-const my = new MyClass();
-
-my.setName('Vlad');
-my.setYear(1971);
-
-console.log(my.getAll);
